ae9fccc7d9bbc9c9bdec6115c161224b2f6833d8
taniTk <tanitk91@gmail.com>
2016-06-17 23:33:07 +0900

    Add abst a word function

--- app/bot.py
+++ app/bot.py
@@ -2,18 +2,20 @@
 # -*- coding: utf-8 -*-
 import re
 import MySQLdb
+from SPARQLWrapper import SPARQLWrapper, JSON
+
 
 class Bot():
     # Please write your code here.
     def __init__(self):
         self.connector = MySQLdb.connect(host='xxxx', db='xxxx', user='xxxx', charset='utf8')
         self.cursor = self.connector.cursor()
-        pass
+        self.sparql = SPARQLWrapper('http://ja.dbpedia.org/sparql')
 
     def recv_message(self, msg):
         msg = msg.strip()
         if re.match('bot', msg) == None or msg == 'bot':
-            return msg
+            return 'None'
         command_list = re.split(' +', msg)[1:]
 
         # ping pong
@@ -23,6 +25,10 @@ class Bot():
         # todo list
         if command_list[0] == 'todo':
             return self.todo(command_list[1:])
+
+        # abstract a word
+        if command_list[0] == 'abst':
+            return self.abst_word(command_list[1])
 
     def todo(self, command_list):
         operator = command_list[0]
@@ -37,13 +43,14 @@ class Bot():
 
     def todo_add(self, command_list):
         if len(command_list) != 2: return 'format error!'
+        command_list = [command.decode('utf-8') for command in command_list]
         sql = u"INSERT INTO todo(title, description) VALUES('{0[0]}','{0[1]}')".format(command_list)
         ans = self.execute_sql(sql)
         if ans == '': ans = 'todo added'
         return ans
 
     def todo_delete(self, command):
-        sql = u"DELETE FROM todo WHERE title='{}'".format(command)
+        sql = u"DELETE FROM todo WHERE title='{}'".format(command.decode('utf-8'))
         ans = self.execute_sql(sql)
         if ans == '': ans = 'todo deleted'
         return ans
@@ -65,8 +72,22 @@ class Bot():
             return 'error!'
         return ''
 
+    def abst_word(self, word):
+        query = u"""
+            PREFIX abst: <http://dbpedia.org/ontology/abstract>
+            SELECT ?x
+            WHERE {{ <http://ja.dbpedia.org/resource/{}> abst: ?x .}}
+        """.format(word.decode('utf-8'))
+        self.sparql.setQuery(query)
+        self.sparql.setReturnFormat(JSON)
+        results = self.sparql.query().convert()
+        return results['results']['bindings'][0]['x']['value']
+
+
 if __name__ == '__main__':
     my_bot = Bot()
-    print my_bot.todo_add(['today', 'aaaaa'])
-    print my_bot.todo_delete('today')
-    print my_bot.todo_list()
+    # print my_bot.todo_add(['today', 'aaaaa'])
+    # print my_bot.todo_delete('today')
+    # print my_bot.todo_list()
+    input = raw_input().decode('utf-8')
+    print my_bot.abst_word(input)
